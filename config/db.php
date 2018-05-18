2c25d0414c4f6215ef87d7635d7f9b1e9d0dbf07
tuowt.cwei <490097962@qq.com>
2018-05-18 09:07:03 +0800

    buil

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=mysql;dbname=yii2_cms',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+];
